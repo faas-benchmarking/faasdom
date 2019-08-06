d88a81251005631eada962ad84290982a5a733d6
Pascal Maissen <pascal.maissen@unifr.ch>
2019-08-06 12:15:56 +0200

    added filesystem test for google in node

---
+++ google/src/node/filesystem/index.js
@@ -0,0 +1,42 @@
+var fs = require("fs");
+var now = require('performance-now');
+
+exports.node_filesystem = (req, res) => {
+  
+  	var instanceId = fs.readFileSync('/proc/self/cgroup', 'utf-8');
+    var cpuinfo = fs.readFileSync('/proc/cpuinfo', 'utf8');
+    var meminfo = fs.readFileSync('/proc/meminfo', 'utf8');
+    var uptime = fs.readFileSync('/proc/uptime', 'utf-8');
+    
+    var text = '';
+    
+    for(let i = 0; i<10240; i++) {
+        text += 'A';
+    }
+    
+    let startWrite = now();
+    for(let i = 0; i<10000; i++) {
+        fs.writeFileSync('/tmp/'+i+'.txt', text, 'utf-8');
+    }
+    let endWrite = now();
+    
+    let startRead = now();
+    for(let i = 0; i<10000; i++) {
+        var test = fs.readFileSync('/tmp/'+i+'.txt', 'utf-8');
+    }
+    let endRead = now();
+    
+    let files = fs.readdirSync('/tmp');
+  
+  	res.set("Content-Type", "application/json");
+	res.status(200);
+    res.send(JSON.stringify({
+      payload: {"test": "filesystem test", "timeWrite(ms)": (endWrite-startWrite).toFixed(3), "timeRead(ms)": (endRead-startRead).toFixed(3)},
+      success: files.length == 10000,
+      n: files.length,
+      cpu: cpuinfo,
+      mem: meminfo,
+      uptime: uptime,
+      memory_avail: process.env.FUNCTION_MEMORY_MB
+    }));
+};
