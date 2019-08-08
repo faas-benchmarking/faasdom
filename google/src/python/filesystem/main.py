0aa0c3f65ce78fb2fb214e7610c3ce52b11b2e70
Pascal Maissen <pascal.maissen@unifr.ch>
2019-08-08 17:24:36 +0200

    google python filesystem parameters added

--- google/src/python/filesystem/main.py
+++ google/src/python/filesystem/main.py
@@ -1,9 +1,13 @@
 import json
 import time
 import os
+import shutil
 
 def python_filesystem(request):
     
+    if os.path.exists("/tmp/test"):
+        shutil.rmtree("/tmp/test")
+
     if not os.path.exists("/tmp/test"):
         os.makedirs("/tmp/test")
         
@@ -21,14 +25,26 @@ def python_filesystem(request):
     if f.mode == 'r':
         uptime =f.read()
     f.close()
+
+    n, size = 10000, 10240
+
+    if request.args.get('n') != None:
+        n = int(request.args.get('n'))
+    else:
+        n = 10000
+
+    if request.args.get('size') != None:
+        size = int(request.args.get('size'))
+    else:
+        size = 10240
     
     text = ""
     
-    for i in range(1, 10240):
+    for i in range(1, size):
         text += "A"
         
     startWrite = time.time()
-    for i in range(0,10000):
+    for i in range(0,n):
         filehandle = open('/tmp/test/'+str(i)+'.txt', 'w')
         filehandle.write(text)
         filehandle.close()
@@ -36,7 +52,7 @@ def python_filesystem(request):
     endWrite = time.time()
     
     startRead = time.time()
-    for i in range(0,10000):
+    for i in range(0,n):
         filehandle = open('/tmp/test/'+str(i)+'.txt', 'r')
         test = filehandle.read()
         filehandle.close()
@@ -50,10 +66,19 @@ def python_filesystem(request):
     }
     
     return (json.dumps({
-        'payload': {"test": "filesystem test", "timeWrite(ms)": (endWrite-startWrite)*1000, "timeRead(ms)": (endRead-startRead)*1000},
-        'success': len(files) == 10000,
-        'n': len(files),
-        'cpu': cpuinfo,
-        'mem': meminfo,
-        'uptime': uptime
-        }), 200, headers)
+        'success': len(files) == n,
+        'payload': {
+            "test": "filesystem test",
+            "n": len(files),
+            "size": size,
+            "timeWrite(ms)": (endWrite-startWrite)*1000,
+            "timeRead(ms)": (endRead-startRead)*1000
+        },
+        'metrics': {
+            'machineId': '',
+            'instanceId': '',
+            'cpu': cpuinfo,
+            'mem': meminfo,
+            'uptime': uptime
+        }
+    }), 200, headers)
