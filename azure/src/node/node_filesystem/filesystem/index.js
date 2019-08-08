8ed79e6d84cff115d73f2d78635fd6112e121c68
Pascal Maissen <pascal.maissen@unifr.ch>
2019-08-08 14:04:34 +0200

    added azure node filesystem params

--- azure/src/node/node_filesystem/filesystem/index.js
+++ azure/src/node/node_filesystem/filesystem/index.js
@@ -1,14 +1,36 @@
 const fs = require("fs");
 const now = require('performance-now');
+var rimraf = require("rimraf");
 
 module.exports = async function (context, req) {
     
-    var instanceId = process.env['WEBSITE_INSTANCE_ID'];
+    var instanceId = fs.readFileSync('/proc/self/cgroup', 'utf-8');
+    var cpuinfo = fs.readFileSync('/proc/cpuinfo', 'utf8');
+    var meminfo = fs.readFileSync('/proc/meminfo', 'utf8');
+    var uptime = fs.readFileSync('/proc/uptime', 'utf-8');
     
+    var n, size;
+
+    if(req.query && req.query.n) {
+        n = req.query.n;
+    } else {
+        n = 10000;
+    }
+
+    if(req.query && req.query.size) {
+        size = req.query.size;
+    } else {
+        size = 10240;
+    }
+
     var text = '';
     
-    for(let i = 0; i<10240; i++) {
+    for(let i = 0; i<size; i++) {
         text += 'A';
+    }
+
+    if(fs.existsSync('/tmp/test')){
+        rimraf.sync('/tmp/test');
     }
 
     if(!fs.existsSync('/tmp/test')){
@@ -16,13 +38,13 @@ module.exports = async function (context, req) {
     }
     
     let startWrite = now();
-    for(let i = 0; i<10000; i++) {
+    for(let i = 0; i<n; i++) {
         fs.writeFileSync('/tmp/test/'+i+'.txt', text, 'utf-8');
     }
     let endWrite = now();
     
     let startRead = now();
-    for(let i = 0; i<10000; i++) {
+    for(let i = 0; i<n; i++) {
         var test = fs.readFileSync('/tmp/test/'+i+'.txt', 'utf-8');
     }
     let endRead = now();
@@ -30,18 +52,26 @@ module.exports = async function (context, req) {
     let files = fs.readdirSync('/tmp/test');
 
     context.res = {
-      statusCode: 200,
-      headers: {
-         "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-          payload: {"test": "filesystem test", "timeWrite(ms)": (endWrite-startWrite).toFixed(3), "timeRead(ms)": (endRead-startRead).toFixed(3)},
-          success: files.length == 10000,
-          n: files.length,
-          id: instanceId,
-          cpu: '',
-          mem: '',
-          uptime: ''
-      })
-  };
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            success: files.length == n,
+            payload: {
+                "test": "filesystem test",
+                "n": files.length,
+                "size": size,
+                "timeWrite(ms)": (endWrite-startWrite).toFixed(3),
+                "timeRead(ms)": (endRead-startRead).toFixed(3)
+            },
+            metrics: {
+                machineId: '',
+                id: instanceId,
+                cpu: cpuinfo,
+                mem: meminfo,
+                uptime: uptime
+            }
+        })
+    };
 };
