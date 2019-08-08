a016e953283c42725b4b2c61c9e05cbb911ab430
Pascal Maissen <pascal.maissen@unifr.ch>
2019-08-08 17:49:21 +0200

    aws go filesystem added paramters

--- aws/src/go/go_filesystem/filesystem.go
+++ aws/src/go/go_filesystem/filesystem.go
@@ -1,121 +1,159 @@
 package main
 
 import (
-        "context"
-        "github.com/aws/aws-lambda-go/lambda"
-        "github.com/aws/aws-lambda-go/events"
-        "io/ioutil"
-        "log"
-        "encoding/json"
-        "strconv"
-        "time"
-        "os"
+    "context"
+    "github.com/aws/aws-lambda-go/lambda"
+    "github.com/aws/aws-lambda-go/events"
+    "io/ioutil"
+    "log"
+    "encoding/json"
+    "strconv"
+    "time"
+    "os"
 )
 
 type Message struct {
-        Payload
-        N string
-        Success bool
-        Cpu string
-        Mem string
-        Uptime string
-        InstanceId string
-    }
+    Success bool
+    Payload
+    Metrics
+
+}
     
-    type Payload struct {
-        Test string
-        TimeWrite string
-        TimeRead string
-    }
+type Payload struct {
+    Test string
+    N int
+    Size int
+    TimeWrite string
+    TimeRead string
+}
+
+type Metrics struct {
+    MachineId string
+    InstanceId string
+    Cpu string
+    Mem string
+    Uptime string
+}
 
 func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-        if _, err := os.Stat("/tmp/test"); os.IsNotExist(err) {
-                os.Mkdir("/tmp/test", 0777)
-        }
+    
+
+    if _, err := os.Stat("/tmp/test"); !os.IsNotExist(err) {
+        os.RemoveAll("/tmp/test")
+    }
+
+    if _, err := os.Stat("/tmp/test"); os.IsNotExist(err) {
+        os.Mkdir("/tmp/test", 0777)
+    }
 
 	buf1, err := ioutil.ReadFile("/proc/self/cgroup")
 	if err != nil {
 		log.Fatal(err)
-        }
-        instanceId := string(buf1)
+    }
+    instanceId := string(buf1)
 
-        buf2, err := ioutil.ReadFile("/proc/cpuinfo")
+    buf2, err := ioutil.ReadFile("/proc/cpuinfo")
 	if err != nil {
 		log.Fatal(err)
-        }
-        cpuinfo := string(buf2)
+    }
+    cpuinfo := string(buf2)
 
-        buf3, err := ioutil.ReadFile("/proc/meminfo")
+    buf3, err := ioutil.ReadFile("/proc/meminfo")
 	if err != nil {
 		log.Fatal(err)
-        }
-        meminfo := string(buf3)
+    }
+    meminfo := string(buf3)
 
-        buf4, err := ioutil.ReadFile("/proc/uptime")
+    buf4, err := ioutil.ReadFile("/proc/uptime")
 	if err != nil {
 		log.Fatal(err)
-        }
-        uptime := string(buf4)
-        
-        var text string = ""
-    
-        for i := 0; i < 10240; i++ {
-            text += "A"
-        }
-    
-        startWrite := time.Now()
-        for i := 0; i < 10000; i++ {
-            f, err := os.Create("/tmp/test/" + strconv.Itoa(i) + ".txt")
-            if err != nil {
+    }
+    uptime := string(buf4)
+
+    n := 10000
+    size := 10240
+
+    if _, ok := event.QueryStringParameters["n"]; ok {
+        n, err = strconv.Atoi(event.QueryStringParameters["n"])
+        if err != nil {
                 log.Fatal(err)
-            }
-            f.WriteString(text)
-            f.Close()
         }
-        elapsedWrite := time.Since(startWrite)
+    } else {
+        n = 10000
+    }
+
+    if _, ok := event.QueryStringParameters["size"]; ok {
+        size, err = strconv.Atoi(event.QueryStringParameters["size"])
+        if err != nil {
+                log.Fatal(err)
+        }
+    } else {
+        size = 10240
+    }
         
-        var test string = ""
+    var text string = ""
     
-        startRead := time.Now()
-        for i := 0; i < 10000; i++ {
-            buf, err := ioutil.ReadFile("/tmp/test/" + strconv.Itoa(i) + ".txt")
-                if err != nil {
-                        log.Fatal(err)
-            }
-            test = string(buf)
-        }
-        elapsedRead := time.Since(startRead)
+    for i := 0; i < size; i++ {
+        text += "A"
+    }
     
-        log.Print(test)
-    
-        files, err := ioutil.ReadDir("/tmp/test")
+    startWrite := time.Now()
+    for i := 0; i < n; i++ {
+        f, err := os.Create("/tmp/test/" + strconv.Itoa(i) + ".txt")
         if err != nil {
             log.Fatal(err)
         }
-        
-        m := &Message{
-                Payload: Payload{
-                    Test: "filesystem test",
-                    TimeWrite: strconv.FormatInt(int64(elapsedWrite / time.Millisecond), 10),
-                    TimeRead: strconv.FormatInt(int64(elapsedRead / time.Millisecond), 10),
-                },
-                N: strconv.Itoa(len(files)),
-                Success: len(files) == 10000,
-                Cpu: cpuinfo,
-                Mem: meminfo,
-                Uptime: uptime,
-                InstanceId: instanceId,
-            }
+        f.WriteString(text)
+        f.Close()
+    }
+    elapsedWrite := time.Since(startWrite)
+    
+    var test string = ""
 
-        js, err := json.Marshal(m)
-        if err != nil {
-                log.Fatal(err)
+    startRead := time.Now()
+    for i := 0; i < n; i++ {
+        buf, err := ioutil.ReadFile("/tmp/test/" + strconv.Itoa(i) + ".txt")
+            if err != nil {
+                    log.Fatal(err)
         }
+        test = string(buf)
+    }
+    elapsedRead := time.Since(startRead)
 
-        return events.APIGatewayProxyResponse{
-		Body:       string(js),
-		StatusCode: 200,
+    log.Print(test)
+
+    files, err := ioutil.ReadDir("/tmp/test")
+    if err != nil {
+        log.Fatal(err)
+    }
+        
+    m := &Message{
+        Success: len(files) == 10000,
+        Payload: Payload{
+            Test: "filesystem test",
+            N: len(files),
+            Size: size,
+            TimeWrite: strconv.FormatInt(int64(elapsedWrite / time.Millisecond), 10),
+            TimeRead: strconv.FormatInt(int64(elapsedRead / time.Millisecond), 10),
+        },
+        Metrics: Metrics{
+            MachineId: "",
+            InstanceId: instanceId,
+            Cpu: cpuinfo,
+            Mem: meminfo,
+            Uptime: uptime,
+        },
+    }
+
+    js, err := json.Marshal(m)
+    if err != nil {
+            log.Fatal(err)
+    }
+
+    return events.APIGatewayProxyResponse{
+    Body:       string(js),
+    StatusCode: 200,
 	}, nil
 }
 
