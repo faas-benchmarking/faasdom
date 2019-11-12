961df38e6884007069b04c677a6df6efdb488334
Pascal Maissen <pascal.maissen@unifr.ch>
2019-11-12 18:34:48 +0100

    complete overwork of pricing module

--- main/pricing.js
+++ main/pricing.js
@@ -1,158 +1,283 @@
-// as of 25.06.2019
+const Influx = require('influx');
 
-// in MB resp. MHz (only Google)
+const influx = new Influx.InfluxDB({
+    host: 'db',
+    port: 8086,
+    database: 'results',
+    username: 'benchmark-suite',
+    password: 'benchmark',
+    schema: []
+});
+
+// in MB resp. GHz (only to be used for Google)
 const functionSizes = [
-    {memory:  128, cpu:  200},
-    {memory:  256, cpu:  400},
-    {memory:  512, cpu:  800},
-    {memory: 1024, cpu: 1400},
-    {memory: 2048, cpu: 2400}
+    {memory:  128, cpu: 0.2},
+    {memory:  256, cpu: 0.4},
+    {memory:  512, cpu: 0.8},
+    {memory: 1024, cpu: 1.4},
+    {memory: 2048, cpu: 2.4}
 ];
 
-// AWS
-// https://aws.amazon.com/de/lambda/pricing/
-// https://aws.amazon.com/de/ec2/pricing/on-demand/
-
-const perCallAWS = 0.0000002;
-const perGBSecAWS = 0.0000166667;
-const perGBNetworkAWS = 0.09;
-
-const freeCallsAWS = 1000000;
-const freeGBSecAWS = 400000;
-const freeGBNetworkAWS = 1;
-
-/** TODO */
-function calcAWSPrice(calls, execTime, kb, index) {
-    
-    var billableCalls = calls - freeCallsAWS > 0 ? calls - freeCallsAWS : 0;
-    var billableGBSec = (calls * execTime * functionSizes[index].memory) / 1024 - freeGBSecAWS > 0 ? (calls * execTime * functionSizes[index].memory) / 1024 - freeGBSecAWS : 0;
-    var billableGBNetwork = (calls * kb / 1024 / 1024) - freeGBNetworkAWS > 0 ? (calls * kb / 1024 / 1024) - freeGBNetworkAWS : 0;
-    var price = (billableCalls * perCallAWS) + (billableGBSec * perGBSecAWS) + (billableGBNetwork * perGBNetworkAWS);
-
-    /*console.log('Invocations: ' + calls + ',\t\t Free: ' + freeCallsAWS + ',\t\t Billed: ' + billableCalls + ',\t\t Cost: ' + (billableCalls * perCallAWS));
-    console.log('GB-seconds: ' + (calls * execTime * functionSizes[index].memory) / 1024 + ',\t\t Free: ' + freeGBSecAWS + ',\t\t Billed: ' + billableGBSec + ',\t\t Cost: ' + (billableGBSec * perGBSecAWS));
-    console.log('Networking: ' + (calls * kb / 1024 / 1024) + ',\t\t Free: ' + freeGBNetworkAWS + ',\t\t Billed: ' + billableGBNetwork + ',\t\t Cost: ' + (billableGBNetwork * perGBNetworkAWS));
-    console.log('Price: ' + Math.round(price * 100) / 100 + '$');
+// as of 11.11.2019 all in USD
+const prices = {
+    /* 
+        AWS updated on 11.11.2019
+        https://aws.amazon.com/de/lambda/pricing/
+        https://aws.amazon.com/de/ec2/pricing/on-demand/
     */
-
-    return price;
-}
-
-
-// Azure
-// https://azure.microsoft.com/en-us/pricing/details/functions/
-// https://azure.microsoft.com/en-us/pricing/details/bandwidth/
-// https://azure.microsoft.com/en-us/pricing/details/storage/files/     INFO: Storage price not included, since it should be practically nothing < 0.10 $
-
-const perCallAzure = 0.0000002;
-const perGBSecAzure = 0.000016;
-const perGBNetworkAzure = 0.087;
-
-const freeCallAzure = 1000000;
-const freeGBSecAzure = 400000;
-const freeGBNetworkAzure = 5;
-
-/** TODO */
-function calcAzurePrice(calls, execTime, kb, index) {
-    
-    var billableCalls = calls - freeCallAzure > 0 ? calls - freeCallAzure : 0;
-    var billableGBSec = (calls * execTime * functionSizes[index].memory) / 1024 - freeGBSecAzure > 0 ? (calls * execTime * functionSizes[index].memory) / 1024 - freeGBSecAzure : 0;
-    var billableGBNetwork = (calls * kb / 1024 / 1024) - perGBNetworkAzure > 0 ? (calls * kb / 1024 / 1024) - perGBNetworkAzure : 0;
-    var price = (billableCalls * perCallAzure) + (billableGBSec * perGBSecAzure) + (billableGBNetwork * perGBNetworkAzure);
-
-    /*console.log('Invocations: ' + calls + ',\t\t Free: ' + freeCallAzure + ',\t\t Billed: ' + billableCalls + ',\t\t Cost: ' + (billableCalls * perCallAzure));
-    console.log('GB-seconds: ' + (calls * execTime * functionSizes[index].memory) / 1024 + ',\t\t Free: ' + freeGBSecAzure + ',\t\t Billed: ' + billableGBSec + ',\t\t Cost: ' + (billableGBSec * perGBSecAzure));
-    console.log('Networking: ' + (calls * kb / 1024 / 1024) + ',\t\t Free: ' + freeGBNetworkAzure + ',\t\t Billed: ' + billableGBNetwork + ',\t\t Cost: ' + (billableGBNetwork * perGBNetworkAzure));
-    console.log('Price: ' + Math.round(price * 100) / 100 + '$');
-    */
-
-    return price;
-}
-
-
-// Google
-// https://cloud.google.com/functions/pricing#invocations
-// https://cloud.google.com/functions/pricing-summary
-
-const perCallGoogle = 0.0000004;
-const perGBSecGoogle = 0.0000025;
-const perGHzSecGoogle = 0.0000100;
-const perGBNetworkGoogle = 0.12;
-
-const freeCallsGoogle = 2000000;
-const freeGBSecGoogle = 400000;
-const freeGHzSecGoogle = 200000;
-const freeGBNetworkGoogle = 5;
-
-/** TODO */
-function calcGooglePrice(calls, execTime, kb, index) {
-    
-    var billableCalls = calls - freeCallsGoogle > 0 ? calls - freeCallsGoogle : 0;
-    var billableGBSec = (calls * execTime * functionSizes[index].memory) / 1024 - freeGBSecGoogle > 0 ? (calls * execTime * functionSizes[index].memory) / 1024 - freeGBSecGoogle : 0;
-    var billableGHzSec = (calls * execTime * functionSizes[index].cpu) / 1000 - freeGHzSecGoogle > 0 ? (calls * execTime * functionSizes[index].cpu) / 1000 - freeGHzSecGoogle : 0;
-    var billableGBNetwork = (calls * kb / 1024 / 1024) - freeGBNetworkGoogle > 0 ? (calls * kb / 1024 / 1024) - freeGBNetworkGoogle : 0;
-    var price = (billableCalls * perCallGoogle) + (billableGBSec * perGBSecGoogle) + (billableGHzSec * perGHzSecGoogle) + (billableGBNetwork * perGBNetworkGoogle);
-
+    aws: {
+        perCall: 0.0000002,
+        perGBSec: 0.0000166667,
+        perGHzSec: 0,
+        perGBNetwork: 0.09,
+        freeCalls: 1000000,
+        freeGBSec: 400000,
+        freeGHzSec: 0,
+        freeGBNetwork: 1,
+    },
     /*
-    console.log('Invocations: ' + calls + ',\t\t Free: ' + freeCallsGoogle + ',\t\t Billed: ' + billableCalls + ',\t\t Cost: ' + (billableCalls * perCallGoogle));
-    console.log('GB-seconds: ' + (calls * execTime * functionSizes[index].memory) / 1024 + ',\t\t Free: ' + freeGBSecGoogle + ',\t\t Billed: ' + billableGBSec + ',\t\t Cost: ' + (billableGBSec * perGBSecGoogle));
-    console.log('GHz-seconds: ' + (calls * execTime * functionSizes[index].cpu) / 1000 + ',\t\t Free: ' + freeGHzSecGoogle + ',\t\t Billed: ' + billableGHzSec + ',\t\t Cost: ' + (billableGHzSec * perGHzSecGoogle));
-    console.log('Networking: ' + (calls * kb / 1024 / 1024) + ',\t\t Free: ' + freeGBNetworkGoogle + ',\t\t Billed: ' + billableGBNetwork + ',\t\t Cost: ' + (billableGBNetwork * perGBNetworkGoogle));
-    console.log('Price: ' + Math.round(price * 100) / 100 + '$');
+        Azure updated on 11.11.2019
+        https://azure.microsoft.com/en-us/pricing/details/functions/
+        https://azure.microsoft.com/en-us/pricing/details/bandwidth/
+        https://azure.microsoft.com/en-us/pricing/details/storage/files/     INFO: Storage price not included, since it should be practically nothing < 0.10 $
     */
-
-    return price;
+    azure: {
+        perCall: 0.0000002,
+        perGBSec: 0.000016,
+        perGHzSec: 0,
+        perGBNetwork: 0.087,
+        freeCalls: 1000000,
+        freeGBSec: 400000,
+        freeGHzSec: 0,
+        freeGBNetwork: 5,
+    },
+    /*
+        Google updated on 11.11.2019
+        https://cloud.google.com/functions/pricing#invocations
+        https://cloud.google.com/functions/pricing-summary
+    */
+    google: {
+        perCall: 0.0000004,
+        perGBSec: 0.0000025,
+        perGHzSec: 0.0000100,
+        perGBNetwork: 0.12,
+        freeCalls: 2000000,
+        freeGBSec: 400000,
+        freeGHzSec: 200000,
+        freeGBNetwork: 5,
+    },
+    /*
+        IBM updated on 11.11.2019
+        https://cloud.ibm.com/functions/learn/pricing
+    */
+    ibm: {
+        perCall: 0,
+        perGBSec: 0.000017,
+        perGHzSec: 0,
+        perGBNetwork: 0,
+        freeCalls: 0,
+        freeGBSec: 400000,
+        freeGHzSec: 0,
+        freeGBNetwork: 0,
+    },
 }
 
+/**
+ * calculate the price
+ * @param {number} calls - the amount of calls
+ * @param {number} execTime - execution time in ms
+ * @param {number} kb - the size of the return body in KB
+ * @param {number} memory - the amount of memory allocated in MB
+ * @param {string} privder - name of the provider ["aws", "azure", "google", "ibm"]
+ */
+function calcPrice(calls, execTime, kb, memory, provider) {
 
-// IBM
-// https://cloud.ibm.com/openwhisk/learn/pricing
+    let invocations_net = (calls-prices[provider].freeCalls) > 0 ? (calls-prices[provider].freeCalls) : 0;
+    let gb_seconds_net = ((execTime/1000*calls*memory)/1024)-prices[provider].freeGBSec > 0 ? ((execTime/1000*calls*memory)/1024)-prices[provider].freeGBSec : 0;
+    let ghz_seconds_net = (execTime/1000*calls*functionSizes[functionSizes.findIndex(x => x.memory == memory)].cpu)-prices[provider].freeGHzSec > 0 ? (execTime/1000*calls*functionSizes[functionSizes.findIndex(x => x.memory == memory)].cpu)-prices[provider].freeGHzSec : 0;
+    let networking_net = (calls*kb/1024/1024)-prices[provider].freeGBNetwork > 0 ? (calls*kb/1024/1024)-prices[provider].freeGBNetwork : 0;
 
-const perGBSecIBM = 0.000017;
+    let ghz_seconds_gross = (execTime/1000*calls*functionSizes[functionSizes.findIndex(x => x.memory == memory)].cpu);
 
-const freeGBSecIBM = 400000;
+    if(provider != "google") {
+        ghz_seconds_net = 0;
+        ghz_seconds_gross = 0;
+    }
 
-/** TODO */
-function calcIBMPrice(calls, execTime, kb, index) {
-    
-    var billableGBSec = (calls * execTime * functionSizes[index].memory) / 1024 - freeGBSecIBM > 0 ? (calls * execTime * functionSizes[index].memory) / 1024 - freeGBSecIBM : 0;
-    var price = (billableGBSec * perGBSecIBM);
+    var table = {
+        provider: provider,
+        execTime: execTime,
+        invocations: {
+            gross: calls,
+            free: prices[provider].freeCalls,
+            net: invocations_net,
+            unit: prices[provider].perCall,
+            total: invocations_net*prices[provider].perCall
+        },
+        gb_seconds: {
+            gross: (execTime/1000*calls*memory)/1024,
+            free: prices[provider].freeGBSec,
+            net: gb_seconds_net,
+            unit: prices[provider].perGBSec,
+            total: gb_seconds_net*prices[provider].perGBSec
+        },
+        ghz_seconds: {
+            gross: ghz_seconds_gross,
+            free: prices[provider].freeGHzSec,
+            net: ghz_seconds_net,
+            unit: prices[provider].perGHzSec,
+            total: ghz_seconds_net*prices[provider].perGHzSec
+        },
+        networking: {
+            gross: (calls*kb/1024/1024),
+            free: prices[provider].freeGBNetwork, 
+            net: networking_net,
+            unit: prices[provider].perGBNetwork,
+            total: networking_net*prices[provider].perGBNetwork
+        },
+        total_price: invocations_net*prices[provider].perCall +
+                     gb_seconds_net*prices[provider].perGBSec +
+                     ghz_seconds_net*prices[provider].perGHzSec +
+                     networking_net*prices[provider].perGBNetwork
+    }
 
-    /*console.log('Invocations: ' + calls);
-    console.log('GB-seconds: ' + (calls * execTime * functionSizes[index].memory) / 1024 + ',\t\t Free: ' + freeGBSecIBM + ',\t\t Billed: ' + billableGBSec + ',\t\t Cost: ' + (billableGBSec * perGBSecIBM));
-    console.log('Price: ' + Math.round(price * 100) / 100 + '$');
-    */
-   
-    return price;
+    return table;
 }
 
+/**
+ * calculate all prices
+ * @param {number} calls - the amount of calls
+ * @param {number} execTime - execution time in ms
+ * @param {number} kb - the size of the return body in KB
+ * @param {number} memory - the amount of memory allocated in MB
+ */
+function calcAllPrices(calls, execTime, kb, memory) {
 
-module.exports = { calcAWSPrice, calcAzurePrice, calcGooglePrice, calcIBMPrice };
+    if(!Number.isInteger(calls) || calls < 0) {
+        return 'ERROR: variable "calls" must be an integer and greater or equal to 0';
+    }
+    if(!Number.isInteger(execTime) || execTime < 0) {
+        return 'ERROR: variable "execTime" must be an integer and greater or equal to 0';
+    }
+    if(!Number.isInteger(kb) || kb < 0) {
+        return 'ERROR: variable "kb" must be an integer and greater or equal to 0';
+    }
+    if(!Number.isInteger(memory) || !(memory != 128 || memory != 256 || memory != 512 || memory != 1024 || memory != 2048)) {
+        return 'ERROR: variable "memory" must be an integer in [128, 256, 512, 1024, 2048]';
+    }
 
+    var result = [];
 
+    var awsTable = calcPrice(calls, execTime, kb, memory, "aws");
+    var azureTable = calcPrice(calls, execTime, kb, memory, "azure");
+    var googleTable = calcPrice(calls, execTime, kb, memory, "google");
+    var ibmTable = calcPrice(calls, execTime, kb, memory, "ibm");
 
+    result.push(awsTable);
+    result.push(azureTable);
+    result.push(googleTable);
+    result.push(ibmTable);
 
-/*var type = 1;
-var calls = 10000000;
-var runtime = 0.5;
-var kb = 5;
+    return result;
 
-console.log('AWS');
-console.log('===')
-calcAWSPrice(calls, runtime, kb, type);
-console.log('');
+}
 
-console.log('Azure');
-console.log('=====')
-calcAzurePrice(calls, runtime, kb, type);
-console.log('');
+/**
+ * calculate all prices
+ * @param {number} calls - the amount of calls
+ * @param {string} testName - name of the test
+ * @param {string} test - name of the test  type ["factors","custom"]
+ * @param {string} runtime - name of runtime ["node","python","go","dotnet"]
+ */
+async function calcAllPricesFromTest(calls, testName, test, runtime) {
 
-console.log('Google');
-console.log('======')
-calcGooglePrice(calls, runtime, kb, type);
-console.log('');
+    if(!Number.isInteger(calls) || calls < 0) {
+        return 'ERROR: variable "calls" must be an integer and greater or equal to 0';
+    }
+    if(!(test != "factors" || test != "custom")) {
+        return 'ERROR: variable "test" must be a string in [factors","memory","filesystem","custom"]';
+    }
+    if(!(runtime != "node" || runtime != "python" || runtime != "go" || runtime != "dotnet")) {
+        return 'ERROR: variable "runtime" must be a string in ["node","python","go","dotnet"]';
+    }
 
-console.log('IBM');
-console.log('===')
-calcIBMPrice(calls, runtime, kb, type);
-console.log('');*/
+    var execTimeAWS = 0;
+    await influx.query(`
+        select mean(measured_ms) from ${test}
+        where language = '${runtime}'
+        and test = '${testName}'
+        and provider = 'aws'
+    `)
+    .then( result => execTimeAWS = result[0].mean )
+    .catch( error => execTimeAWS = -1 );
+
+    var execTimeAzure = 0;
+    await influx.query(`
+        select mean(measured_ms) from ${test}
+        where language = '${runtime}'
+        and test = '${testName}'
+        and provider = 'azure'
+    `)
+    .then( result => execTimeAzure = result[0].mean )
+    .catch( error => execTimeAzure = -1 );
+
+    var execTimeAzureWindows = 0;
+    await influx.query(`
+        select mean(measured_ms) from ${test}
+        where language = '${runtime}'
+        and test = '${testName}'
+        and provider = 'azureWindows'
+    `)
+    .then( result => execTimeAzureWindows = result[0].mean )
+    .catch( error => execTimeAzureWindows = -1 );
+
+    var execTimeGoogle = 0;
+    await influx.query(`
+        select mean(measured_ms) from ${test}
+        where language = '${runtime}'
+        and test = '${testName}'
+        and provider = 'google'
+    `)
+    .then( result => execTimeGoogle = result[0].mean )
+    .catch( error => execTimeGoogle = -1 );
+
+    var execTimeIBM = 0;
+    await influx.query(`
+        select mean(measured_ms) from ${test}
+        where language = '${runtime}'
+        and test = '${testName}'
+        and provider = 'ibm'
+    `)
+    .then( result => execTimeIBM = result[0].mean )
+    .catch( error => execTimeIBM = -1 );
+
+    var memory = 0;
+    await influx.query(`
+        select first(measured_ms), memory from ${test}
+        where language = '${runtime}'
+        and test = '${testName}'
+    `)
+    .then( result => memory = result[0].memory )
+    .catch( error => memory = -1 );
+
+    var kb = 1;
+
+    var result = [];
+
+    var awsTable = calcPrice(calls, execTimeAWS, kb, memory, "aws");
+    var azureTable = calcPrice(calls, execTimeAzure, kb, memory, "azure");
+    var azureWindowsTable = calcPrice(calls, execTimeAzureWindows, kb, memory, "azure");
+    var googleTable = calcPrice(calls, execTimeGoogle, kb, memory, "google");
+    var ibmTable = calcPrice(calls, execTimeIBM, kb, memory, "ibm");
+
+    result.push(awsTable);
+    result.push(azureTable);
+    result.push(azureWindowsTable);
+    result.push(googleTable);
+    result.push(ibmTable);
+
+    return result;
+
+}
+
+module.exports = { calcAllPrices, calcAllPricesFromTest };
